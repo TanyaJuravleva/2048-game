14c0fe5d4671f7372ac3d77aa37898646341caea
Tanya <krilltt@mail.ru>
2025-02-04 18:49:10 +0300

    edit bat file

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-docker build -t game2048 .
-start docker run -d -p 8081:8080 game2048
-start docker run -d -p 8082:8080 game2048
+docker build -t game2048:16 .
+start docker run -d -p 8081:8080 game2048:16
+start docker run -d -p 8082:8080 game2048:16
